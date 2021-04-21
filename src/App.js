92475a4aa721dcbfc43a8c9c80a823d5ab2944d4
Nilton Joaquin <nilton.joaquin@gmail.com>
2021-04-21 16:34:29 -0500

    INT-1: new Project react from scratch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+import MiComponente from './components/MiComponente';
 
+function HolaMundo(nombre,edad){
+  var presentacion= (<div>
+    <h2>Hola, soy {nombre}</h2>
+    <h3>Tengo {edad} años</h3>
+  </div>);
+  return presentacion;
+}
 function App() {
+  var nombre = "Nilton J.";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola Bienvenido al curso de Nilton Joaquin
+          4545
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {HolaMundo(nombre,33)}
+        <section className="componentes">
+        <MiComponente />
+        <MiComponente />
+        <MiComponente />
+      </section>
       </header>
+    
+      
+      
     </div>
   );
 }
